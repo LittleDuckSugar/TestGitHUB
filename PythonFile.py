616d533eba7f73c89ea60bf8cc8e863bf2e184a6
LittleDuckSugar <65243261+LittleDuckSugar@users.noreply.github.com>
2020-05-22 18:13:28 +0200

    Added update content of README file

--- PythonFile.py
+++ PythonFile.py
@@ -7,6 +7,7 @@ print("Think that pull request is understood")
 print("Fixed the issue #2")
 print("It looks like that <hr> balise and '---' in the README.md file do the same thing")
 print("Replacing every '---' with <hr> balise for better HTML understanding")
+print("General enhancement of the README.md file")
 
 message = "Hope you enjoy!"
 
